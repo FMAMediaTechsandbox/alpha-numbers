e475ad91c287951c113a638f458fcb9184d6f82f
FMAMediaTechsandbox <142332170+FMAMediaTechsandbox@users.noreply.github.com>
2023-08-22 12:46:45 +0700

    up

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,3 +1,6 @@
+// Menangkap class menu-toggle
 const menuToggle = document.querySelector('.menu-toggle');
+// Menangkap element nav dan ul
 const nav = document.querySelector('nav ul');
+// Menambahkan event ketika di klik maka tambahkan kelas slide, jika user klik lagi maka hapus class slide
 menuToggle.addEventListener('click', () => { nav.classList.toggle('slide'); });
