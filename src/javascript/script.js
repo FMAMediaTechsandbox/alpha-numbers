22d8bf031f0316d953e51a8a95c88cfe87b91d1a
FMA <111466154+FMATheNomad@users.noreply.github.com>
2023-08-30 09:49:17 +0700

    Fixing form contacts.

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -4,3 +4,9 @@ const menuToggle = document.querySelector('.menu-toggle');
 const nav = document.querySelector('nav ul');
 // Menambahkan event ketika di klik maka tambahkan kelas slide, jika user klik lagi maka hapus class slide
 menuToggle.addEventListener('click', () => { nav.classList.toggle('slide'); });
+// Dapatkan elemen <p> dengan ID "copyright"
+const copyrightElement = document.getElementById("copyright");
+// Dapatkan tahun saat ini
+const currentYear = new Date().getFullYear();
+// Set teks pada elemen
+copyrightElement.textContent = `Copyright © ${currentYear} FMA MEDIA TECHNOLOGIES. All rights reserved.`;
